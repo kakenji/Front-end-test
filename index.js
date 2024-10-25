35da82962078f2ed5938a5e74dba03ff29e2fa1a
Kayky Kenji <kaykyjk01@gmail.com>
2024-10-25 09:57:42 -0300

    Config index.ejs, style.css and index.js

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// app.use(routes);
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public")));
-// app.get('/', (req, res) => {
-//     res.render('index');
-// })
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', { numbers: [] });
@@ -28,5 +25,5 @@ app.post('/rows', (req, res) => {
     res.render('index', { numbers });
 
 })
-app.set('view engine', 'ejs');
+
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-const routes = require('express').Router();
-const TaskController = require('../controller/TaskController');
-
-routes.get('/', TaskController.getInput);
-
-module.exports = routes;
